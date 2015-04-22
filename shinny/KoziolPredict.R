1c56bfbaaed08eb953827c45f30e265cb7c1c984
xush4 <xush4@sina.com>
2015-04-22 13:45:59 +0800

    Shiny App

---
+++ shinny/KoziolPredict.R
@@ -0,0 +1,81 @@
+library(data.table)
+
+load("ProbabilityTable.rds")
+load("uniqueWords.RData")
+load("profanity.rds")
+
+parseString <- function(thePhrase){
+  SD1 <- tolower(thePhrase)
+  #remove profanity
+  SD1 <- gsub(paste(profanity, collapse='|'), " ", SD1)
+  #collapse all ' to join words
+  SD1 <- gsub("'", "", SD1)
+  SD1 <- gsub("-", "", SD1)
+  #turn all characters not in the alphabet or a space into a space
+  SD1 <- gsub("[^a-z ]", " ", SD1) 
+  #collapse all multiple spaces into one space
+  SD1 <- gsub(" {2,}", " ", SD1)
+  
+  SD1split <- strsplit(SD1, " ")
+  SD1splitlength <- length(SD1split[[1]])
+  if(SD1splitlength > 2){
+    return(SD1split[[1]][(SD1splitlength-2):SD1splitlength])
+  }
+  else{
+    
+    return(SD1split[[1]][1:SD1splitlength])
+  }
+}
+
+filterUnknownWords <- function(wordArray){
+  for(i in 1:length(wordArray)){
+    if(!(wordArray[i] %in% uniqueWords)){
+      wordArray[i] <- "<unk>"
+    }
+  }
+  
+  return(wordArray)
+}
+
+createNGrams <- function(wordArray){
+  ngramsToFind <- c()
+  wordArrayLength <- length(wordArray)
+  for(i in 1:wordArrayLength){
+    ngramsToFind <- c(ngramsToFind, paste(wordArray[i:wordArrayLength], collapse=" "))
+  }
+  return(ngramsToFind)
+  
+}
+
+findTopWords <- function(ngramArray, probTable, n){
+  selectedWords <- probTable[probTable$firstWord %in% ngramArray,]
+  if(!(length(selectedWords) > 0)){
+    selectedWords <- predictThis("<unk> <unk> <unk>")
+  }
+  
+  
+  aggWords <- aggregate(V1 ~ lastWord, data=selectedWords, sum)
+  aggWords <- aggWords[with(aggWords, order(-V1)),]
+  
+  return(head(aggWords, n))
+  
+}
+
+predictThis <- function(phrase){
+  if(nchar(phrase) > 0){
+    pred <- parseString(phrase)
+    pred <- filterUnknownWords(pred)
+    pred <- createNGrams(pred)
+    pred <- findTopWords(pred, ProbabilityTable, 10)
+    
+    return(pred$lastWord)
+  }
+  else{
+    pred <- parseString("<unk>")
+    pred <- filterUnknownWords(pred)
+    pred <- createNGrams(pred)
+    pred <- findTopWords(pred, ProbabilityTable, 10)
+    
+    return(pred$lastWord)
+  }
+}
---
+++ shinny/ui.R
@@ -0,0 +1,21 @@
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Next Word Text Prediction"),
+  
+  sidebarPanel(
+    p('The purpose of this app is to predict the next word after a given amount of text.  
+      A predicted word will be provided after typing along with other possible words and their
+      respective probabilities')
+    
+    
+    
+  ),
+  mainPanel(
+    h3('Begin Typing in the Box Below'),
+    textInput("userText", "",""),
+    textOutput("firstWord"),
+    textOutput("secondWord"),
+    textOutput("thirdWord"),
+    textOutput("otherWords")
+  )
+))
